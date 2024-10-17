15b4567482d658994527b97965db6c760bd70724
ymelnik <yuliia.melnik@capgemini.com>
2024-10-18 00:06:25 +0200

    change application package

--- src/main/java/com/retail/store/StoreApplication.java
+++ src/main/java/com/retail/store/StoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.inditex.store"})
+@ComponentScan(basePackages = {"com.retail.store"})
 public class StoreApplication {
 
   public static void main(String[] args) {
