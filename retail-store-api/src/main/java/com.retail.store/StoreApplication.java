9b1ca34b58fb298f2fd82378485d3d141c8fb03c
ymelnik <yuliia.melnik@capgemini.com>
2024-10-17 21:45:16 +0200

    feature/TASK-01-create-api create retail store application

---
+++ retail-store-api/src/main/java/com.retail.store/StoreApplication.java
@@ -0,0 +1,15 @@
+package com.retail.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.retail.store"})
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
